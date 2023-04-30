4ae7fcf3b7bc786ce6ce23298319454a66e7f960
Emil Olszewski <268789@student.pwr.edu.pl>
2023-04-30 21:03:43 +0200

    Dodano benchmarki

--- zadania/zad1.jl
+++ zadania/zad1.jl
@@ -1,10 +1,12 @@
 # Zadanie 1
 using Plots 
+using BenchmarkTools
 
+# Generator liczb pseudolosowych o rozkładzie U(0, 1)
 function MRG32k3a(
     size, 
-    seedx = (1822406609, 1153072917, 1254937013), 
-    seedy = (2311980605, 7239483046, 2394024690)
+    seedx = [1822406609, 1153072917, 1254937013], 
+    seedy = [2311980605, 7239483046, 2394024690]
     )
 
     result = Vector{Float64}(undef, size)
@@ -12,19 +14,15 @@ function MRG32k3a(
     m1 = 2^32 - 209
     m2 = 2^32 - 22853
 
-    prevX :: Vector{Int64} = [seedx...]
-    prevY :: Vector{Int64} = [seedy...]
-
     for i in 1:size
 
-        x = mod((1403580prevX[2] - 810728prevX[3]), m1)
-        y = mod((527612prevY[1] - 1370589prevY[3]), m2)
+        x = mod((1403580seedx[2] - 810728seedx[3]), m1)
+        y = mod((527612seedy[1] - 1370589seedy[3]), m2)
 
-        # result[i] = x <= y ? (x - y + m1) / (m1 + 1) : (x - y) / (m1 + 1)
         result[i] = mod((x - y), m1) / m1
 
-        pop!(prevX); pop!(prevY)
-        pushfirst!(prevX, x); pushfirst!(prevY, y)
+        pop!(seedx); pop!(seedy)
+        pushfirst!(seedx, x); pushfirst!(seedy, y)
 
     end
 
@@ -32,10 +30,14 @@ function MRG32k3a(
 
 end
 
-samples = MRG32k3a(10000)
+@btime MRG32k3a(10_000)
+
+samples = MRG32k3a(100_000)
+# Rozkład odległości między realizacjami
 distances = [abs(samples[i + 1] - samples[i]) for i in 1:length(samples)-1]
 
-scatter(1:length(samples), samples)
-scatter(1:length(samples)-1, distances)
+scatter(1:length(samples), samples, markersize=1)
+scatter(1:length(samples)-1, distances, markersize=1)
 
 histogram(samples, normalize=:pdf)
+histogram(distances, normalize=:pdf)
