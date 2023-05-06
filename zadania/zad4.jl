4c5c97811cafa8dde0e2e37fe8605f3af5ccfc06
Emil Olszewski <268789@student.pwr.edu.pl>
2023-05-07 00:39:12 +0200

    metoda boxa-mullera, marsaglii, odwrotnej dystrybuanty

---
+++ zadania/zad4.jl
@@ -0,0 +1,94 @@
+# Zadanie 4
+
+using Plots 
+using Distributions
+using BenchmarkTools
+using HypothesisTests
+using SpecialFunctions
+
+SAMPLESIZE = 100
+
+###############################
+# Generowanie wbudowaną funkcją
+###############################
+
+@btime randn(SAMPLESIZE)
+
+builtin_sample = randn(SAMPLESIZE)
+ExactOneSampleKSTest(builtin_sample, Normal(0, 1))
+
+
+# Dystrybuanta empiryczna
+builtin_sorted = sort(builtin_sample)
+plot(builtin_sorted, LinRange(0, 1, SAMPLESIZE), linetype=:steppre)
+
+#################################
+# Generowanae metodą Boxa-Mullera
+#################################
+
+function boxmuller(size)
+
+    θ = 2π .* rand(size)
+    R = sqrt.(-2 .* log.(rand(size)))
+
+    return R .* sin.(θ)
+end
+
+@btime boxmuller(SAMPLESIZE)
+
+boxmuller_sample = boxmuller(SAMPLESIZE)
+ExactOneSampleKSTest(boxmuller_sample, Normal(0, 1))
+
+boxmuller_sorted = sort(boxmuller_sample)
+plot(boxmuller_sorted, LinRange(0, 1, SAMPLESIZE), linetype=:steppre)
+
+
+##############################
+# Generowanie metodą Marsaglii
+##############################
+
+function marsaglia(size)
+
+    result = Vector{Float64}(undef, size)
+
+    for i in 1:size
+        x = 2 * rand() - 1
+        y = 2 * rand() - 1
+
+        while x^2 + y^2 > 1
+            x = 2 * rand() - 1
+            y = 2 * rand() - 1
+        end
+
+        s = x^2 + y^2
+
+        result[i] = x * sqrt(-2log(s) / s)
+    end
+
+    return result
+end
+
+@btime marsaglia(SAMPLESIZE)
+
+marsaglia_samples = marsaglia(SAMPLESIZE)
+ExactOneSampleKSTest(marsaglia_samples, Normal(0, 1))
+
+marsaglia_sorted = sort(marsaglia_samples)
+plot(marsaglia_sorted, LinRange(0, 1, SAMPLESIZE), linetype=:steppre)
+
+###########################################
+# Generowanie metodą odwrotnej dystrybuanty
+###########################################
+
+
+inversecdf(x) = -sqrt(2) * erfcinv(2x)
+
+function invΦ(size)
+    return inversecdf.(rand(size))
+end
+
+@btime invΦ(SAMPLESIZE)
+
+
+inversecdf_samples = invΦ(SAMPLESIZE)
+ExactOneSampleKSTest(inversecdf_samples, Normal(0, 1))
